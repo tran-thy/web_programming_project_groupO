5f9dc819a87c199b58fe9605ba117fe835b85992
Ha Nguyen <c3ngth01@student.oamk.fi>
2024-04-15 02:17:01 +0300

    update homepage

--- utils/navbar.js
+++ utils/navbar.js
@@ -1,5 +1,8 @@
-$(document).ready(function () {
-  $("#toggle").click(function () {
-    $(".navbar").slideToggle();
+document.addEventListener("DOMContentLoaded", function () {
+  var toggleButton = document.getElementById("toggle");
+  var navbar = document.querySelector(".navbar");
+
+  toggleButton.addEventListener("click", function () {
+    navbar.classList.toggle("open");
   });
 });
