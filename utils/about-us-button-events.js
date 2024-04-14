48c1cdfd7602518b95df14cb3631e74db33fb8ca
thy.tran <kathytran11@gmail.com>
2024-04-14 17:21:42 +0300

    Modify About button in footer

--- utils/about-us-button-events.js
+++ utils/about-us-button-events.js
@@ -11,33 +11,35 @@ document.getElementById("about-btn").addEventListener("click", function() {
             document.getElementById('myCarousel').style.display = 'none';
             document.getElementById('search-results-section').style.display = 'none';
             document.getElementById('vn-recipe-display-section').style.display = 'none';
+            document.getElementById('cn-recipe-display-section').style.display = 'none';
         });
 });
 
-document
-    .getElementById("carousel-btn-about")
-    .addEventListener("click", function() {
-        const aboutUsSection = document.getElementById("about-us-section");
+document.getElementById("carousel-btn-about").addEventListener("click", function() {
+    const aboutUsSection = document.getElementById("about-us-section");
 
-        fetch("a-about-us-content.html")
-            .then((response) => response.text())
-            .then((data) => {
-                aboutUsSection.innerHTML = data;
-                document.getElementById("highlighted-section").style.display = "none";
-                document.getElementById("myCarousel").style.display = "none";
-            });
-    });
+    fetch("a-about-us-content.html")
+        .then((response) => response.text())
+        .then((data) => {
+            aboutUsSection.innerHTML = data;
+            document.getElementById("highlighted-section").style.display = "none";
+            document.getElementById("myCarousel").style.display = "none";
+        });
+});
 
-document
-    .getElementById("footer-btn-about")
-    .addEventListener("click", function() {
-        const aboutUsSection = document.getElementById("about-us-section");
+document.getElementById("footer-btn-about").addEventListener("click", function() {
+    const aboutUsSection = document.getElementById("about-us-section");
 
-        fetch("a-about-us-content.html")
-            .then((response) => response.text())
-            .then((data) => {
-                aboutUsSection.innerHTML = data;
-                document.getElementById("highlighted-section").style.display = "none";
-                document.getElementById("myCarousel").style.display = "none";
-            });
-    });
+    fetch("a-about-us-content.html")
+        .then((response) => response.text())
+        .then((data) => {
+            aboutUsSection.innerHTML = data;
+
+            // Hide the section
+            document.getElementById('highlighted-section').style.display = 'none';
+            document.getElementById('myCarousel').style.display = 'none';
+            document.getElementById('search-results-section').style.display = 'none';
+            document.getElementById('vn-recipe-display-section').style.display = 'none';
+            document.getElementById('cn-recipe-display-section').style.display = 'none';
+        });
+});
