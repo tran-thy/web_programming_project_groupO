f9160cdef40b550907957bd917543c45f0bed9e0
Yixin Gao <yexijun7gaoyixin@gmail.com>
2024-04-22 11:35:03 +0300

    6

--- login/login.sql
+++ login/login.sql
@@ -1,9 +1,16 @@
-drop table if exists login;
+-- drop table if exists login;
 
-CREATE TABLE login (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+-- CREATE TABLE login (
+--     id SERIAL PRIMARY KEY,
+--     email VARCHAR(255) NOT NULL,
+--     username VARCHAR(50) NOT NULL,
+--     password VARCHAR(255) NOT NULL
+-- );
 -- insert into login (email, username, password) values ('123@qq.com', 'name123', 'password123');
+drop table if exists login;
+CREATE TABLE login (
+    ID SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL
+);
