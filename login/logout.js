02c246d896fe168114b4dc5605868ead30d06966
Yixin Gao <yexijun7gaoyixin@gmail.com>
2024-04-24 11:46:32 +0300

    10

--- login/logout.js
+++ login/logout.js
@@ -1,17 +1,13 @@
 import { User } from "./Users.js";
 
-const user = new User()
-const email_input = document.querySelector('#user-email')
-const password_input = document.querySelector('#password')
+const user = new User();
 
-document.querySelector('#confirm-button').addEventListener('click',(event) => {
-  event.preventDefault()
-  const email = email_input.value
-  const password = password_input.value
-user.logout(email, password).then(user => {
-  window.location.href = '../index.html'
-}).catch((error) => {
-  alert(error)})
+document.addEventListener('DOMContentLoaded', function () {
+    const login_link = document.querySelector('.return-link');
 
-  
-})
+
+    user.logout();
+
+    login_link.innerHTML = "Login";
+    login_link.href = "login.html";
+});
