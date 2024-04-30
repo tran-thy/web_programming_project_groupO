d2bd6a25cf3cf73f9b09539c0d41c061fd4cb3cb
Ha Nguyen <c3ngth01@student.oamk.fi>
2024-04-30 14:15:11 +0300

    commit

--- server/db.sql
+++ server/db.sql
@@ -794,7 +794,7 @@ CREATE TABLE comments (
     user_id INT NOT NULL,
     content TEXT NOT NULL,
     FOREIGN KEY (dishID) REFERENCES Vietnamese_Recipes(dishid) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES login(id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
