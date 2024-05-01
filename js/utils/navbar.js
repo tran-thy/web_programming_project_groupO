fbf7fe4b1e5f8574b2c0411a3654eed09cb096e9
HaNguyen-oamk <156862834+HaNguyen-oamk@users.noreply.github.com>
2024-05-01 20:27:22 +0300

    Delete js/utils/navbar.js

--- js/utils/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var toggleButton = document.getElementById("toggle");
-  var navbar = document.querySelector(".navbar");
-
-  toggleButton.addEventListener("click", function () {
-    navbar.classList.toggle("open");
-  });
-});
